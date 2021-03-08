c32275d79c3c67fd2bb9385795c1907f8b0afc4f
KevalBavadiya <60056875+KevalBavadiya@users.noreply.github.com>
2021-03-08 11:26:17 +0530

    Add files via upload

---
+++ main (11).cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string x1,x2;
+    int a,b;
+    cin>>x1;
+    cin>>x2;
+    a=x1.length();
+    b=x2.length();
+    cout<<a;
+    cout<<b;
+    return 0;
+}
